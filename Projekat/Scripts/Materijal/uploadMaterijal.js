c881a8c1e410dc1f438ead54aad2f1ef5165dfe4
Bojan <culibrkbojan98@gmail.com>
2020-05-13 12:06:24 +0200

    SnackBar

--- Projekat/Scripts/Materijal/uploadMaterijal.js
+++ Projekat/Scripts/Materijal/uploadMaterijal.js
@@ -1,4 +1,13 @@
 ﻿$(document).ready(function () {
+    sessionStorage.setItem('MatCreate', true);
+    var data = sessionStorage.getItem('MatCreate');
+    if (data) {
+        $('#snackbar').css('display', 'block');
+        sessionStorage.removeItem('MatCreate');
+    }
+    else
+        $('#snackbar').css('display', 'none');
+
     $(".customSelect").trigger('change');
 
     var predmetId = sessionStorage.getItem('predmetId');
@@ -8,80 +17,74 @@
     else
         $("#Materijal_predmetId").val($('#Materijal_predmetId option').first().val());
 
-    var isUploaded = sessionStorage.getItem('upload');
+    var forma = $('#postavkaMat');
+    var modulId = $('#modulId').find($("option:selected")).val();
+    $('#modulId').on('change', function () {
+        modulId = $('#modulId').find($("option:selected")).val();
+    })
 
-    if (isUploaded) {
-        $('#snackbar').css('display', 'block');
-        sessionStorage.removeItem('upload');
-    }
-    else
-        $('#snackbar').css('display', 'none');
+    $(forma).validate({
+        rules: {
+            "Materijal.materijalNaslov": {
+                required: true
+            },
+            "Materijal.materijalOpis": {
+                required: true
+            },
+            file: {
+                required: true,
+
+                extension: "jpeg|png|jpg|pdf|mp4|rtf|txt|text|gif|rar|zip"
+                ,
+                maxFileSize: {
+                    "unit": "KB",
+                    "size": 5000
+                },
+                minFileSize: {
+                    "unit": "KB",
+                    "size": "1"
+                }
+            }
+        },
+        messages: {
+            "Materijal.materijalNaslov": {
+                required: "Polje naslov je obavezno."
+            },
+            "Materijal.materijalOpis": {
+                required: "Polje opis je obavezno."
+            },
+            file: {
+                required: "Morate odabrati fajl.",
+                extension: "Pogrešan format fajla.",
+                maxFileSize: "Fajl ne sme biti veći od 5MB",
+                minFileSize: "Fajl ne sme biti manji od 1KB"
+            }
+        },
+        errorPlacement: function (error, element) {
+            if (element.attr("name") == "file") {
+                error.insertAfter(element.next());
+            }
+            else {
+                error.insertAfter(element);
+            }
+        },
+        submitHandler: function (forma) {
+            sessionStorage.setItem('MatCreate', true);
+            $.ajax({
+                method: 'POST',
+                url: '/Materijal/UplaodMaterijal',
+                data: {
+                    id: predmetId,
+                    modulId: modulId,
+                    sort: sort,
+                    tipovi: tipovi,
+                    formati: formati
+                },
+            });
+
+            forma.submit();
+        }
+    })
+
 });
 
-var forma = $('#postavkaMat');
-var modulId = $('#modulId').find($("option:selected")).val();
-$('#modulId').on('change', function () {
-    modulId = $('#modulId').find($("option:selected")).val();
-})
-
-$('#postavkaMat').validate({
-    rules: {
-        "Materijal.materijalNaslov": {
-            required: true
-        },
-        "Materijal.materijalOpis": {
-            required: true
-        },
-        file: {
-            required: true,
-
-            extension: "jpeg|png|jpg|pdf|mp4|rtf|txt|text|gif|rar|zip"
-            ,
-            maxFileSize: {
-                "unit": "KB",
-                "size": 5000
-            },
-            minFileSize: {
-                "unit": "KB",
-                "size": "1"
-            }
-        }
-    },
-    messages: {
-        "Materijal.materijalNaslov": {
-            required: "Polje naslov je obavezno."
-        },
-        "Materijal.materijalOpis": {
-            required: "Polje opis je obavezno."
-        },
-        file: {
-            required: "Morate odabrati fajl.",
-            extension: "Pogrešan format fajla.",
-            maxFileSize: "Fajl ne sme biti veći od 5MB",
-            minFileSize: "Fajl ne sme biti manji od 1KB"
-        }
-    },
-    errorPlacement: function (error, element) {
-        if (element.attr("name") == "file") {
-            error.insertAfter(element.next());
-        }
-        else {
-            error.insertAfter(element);
-        }
-    },
-    submitHandler: function (forma) {
-        $.ajax({
-            method: 'POST',
-            url: '/Materijal/UplaodMaterijal',
-            data: {
-                id: predmetId,
-                modulId: modulId,
-                sort: sort,
-                tipovi: tipovi,
-                formati: formati
-            },
-        });
-        sessionStorage.setItem('upload', true);
-        forma.submit();
-    }
-})
